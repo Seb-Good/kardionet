f570e0a5602976966083d826ba1c86d16f50b730
rosie <sebi.goodfellow@gmail.com>
2019-04-26 20:30:21 -0400

    - Completed visualization module for records - Added example figure to README

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 Automatic ECG segmentation using deep learning.
 
 ### The QT Database
-The [QT Database](https://physionet.org/physiobank/database/qtdb/) includes ECGs which were chosen to represent a wide variety of QRS and ST-T morphologies, in order to 
-challenge QT detection algorithms with real-world variability. The QT Database contains a total of 105 fifteen-minute 
-excerpts of two channel ECGs, selected to avoid significant baseline wander or other artifacts. 
+The [QT Database](https://physionet.org/physiobank/database/qtdb/) includes ECGs which were chosen to represent a wide 
+variety of QRS and ST-T morphologies, in order to challenge QT detection algorithms with real-world variability. The QT 
+Database contains a total of 105 fifteen-minute excerpts of two channel ECGs, selected to avoid significant baseline 
+wander or other artifacts. 
 
 #### [Download Link](https://www.physionet.org/physiobank/database/qtdb/)
+
+#### Label Example
+![Label Example](documents/README/label_example.png)
 
 ## License
 [MIT](LICENSE.txt)
--- kardionet/visualizations/records.py
+++ kardionet/visualizations/records.py
@@ -8,7 +8,10 @@ By: Sebastian D. Goodfellow and Noel Kippers, 2019
 # 3rd party imports
 import os
 import numpy as np
+from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm
 from ipywidgets import interact, fixed
 from ipywidgets.widgets import IntSlider
 
@@ -26,18 +29,18 @@ def plot_record(record_name_id, record_names):
     record = Record(record_name=record_name)
 
     # Setup figure
-    fig = plt.figure(figsize=(15, 15))
+    fig = plt.figure(figsize=(15, 20), facecolor='w')
     fig.subplots_adjust(wspace=0, hspace=0.3)
-    ax1 = plt.subplot2grid((3, 1), (0, 0))
-    ax2 = plt.subplot2grid((3, 1), (1, 0))
-    ax3 = plt.subplot2grid((3, 1), (2, 0))
+    ax1 = plt.subplot2grid((4, 1), (0, 0))
+    ax2 = plt.subplot2grid((4, 1), (1, 0))
+    ax3 = plt.subplot2grid((4, 1), (2, 0))
+    ax4 = plt.subplot2grid((4, 1), (3, 0))
 
     # Get time array
     time = np.arange(record.waveforms.shape[0]) * 1 / record.fs
 
     # Plot channel 1
-    ax1.set_title('Intervals: {}\nLabels: {}'.format(record.num_intervals, len(record.labels)),
-                  fontsize=20, y=1.02, loc='left')
+    ax1.set_title('Labeled Intervals: {}'.format(record.num_intervals), fontsize=20, y=1.02, loc='left')
     ax1.plot(time, record.waveforms[:, 0], '-', color=[0.7, 0.7, 0.7], lw=2)
     for interval in record.intervals_df['interval'].unique():
         ax1.plot(time[record.intervals_df['index'][record.intervals_df['interval'] == interval]],
@@ -46,12 +49,11 @@ def plot_record(record_name_id, record_names):
     ax1.set_xlabel('Time, s', fontsize=22)
     ax1.set_ylabel('Ch1 Amplitude', fontsize=22)
     ax1.set_xlim([time.min(), time.max()])
-    #ax1.axes.get_xaxis().set_visible(False)
-    #ax1.tick_params(labelbottom='off')
     ax1.xaxis.set_tick_params(labelsize=16)
     ax1.yaxis.set_tick_params(labelsize=16)
 
     # Plot channel 2
+    ax2.set_title('Labeled Intervals: {}'.format(record.num_intervals), fontsize=20, y=1.02, loc='left')
     ax2.plot(time, record.waveforms[:, 1], '-', color=[0.7, 0.7, 0.7], lw=2)
     for interval in record.intervals_df['interval'].unique():
         ax2.plot(time[record.intervals_df['index'][record.intervals_df['interval'] == interval]],
@@ -63,17 +65,44 @@ def plot_record(record_name_id, record_names):
     ax2.xaxis.set_tick_params(labelsize=16)
     ax2.yaxis.set_tick_params(labelsize=16)
 
-    # Plot labels
-    ax3.set_title('Intervals', fontsize=20, y=1.02, loc='left')
-    ax3.plot(record.intervals_df['ch1'], '-k')
-    ax3.set_xlabel('Samples', fontsize=22)
-    ax3.set_ylabel('Ch2 Amplitude', fontsize=22)
-    ax3.set_xlim([0, record.intervals_df['ch1'].shape[0]])
+    # Plot channel 1 labels
+    time = np.arange(record.intervals_df.shape[0]) * 1 / record.fs
+    ax3.set_title('Beat Labels: {}'.format(len(record.labels)), fontsize=20, y=1.02, loc='left')
+    generate_line_plot(x=time, y=record.intervals_df['ch1'].values, z=record.intervals_df['train_label'].values, ax=ax3)
+    ax3.set_xlabel('Time, s', fontsize=22)
+    ax3.set_ylabel('Ch1 Amplitude', fontsize=22)
+    ax3.set_xlim([time.min(), time.max()])
     ax3.xaxis.set_tick_params(labelsize=16)
     ax3.yaxis.set_tick_params(labelsize=16)
 
+    # Plot channel 2 labels
+    ax4.set_title('Beat Labels: {}'.format(len(record.labels)), fontsize=20, y=1.02, loc='left')
+    generate_line_plot(x=time, y=record.intervals_df['ch2'].values, z=record.intervals_df['train_label'].values, ax=ax4)
+    ax4.set_xlabel('Time, s', fontsize=22)
+    ax4.set_ylabel('Ch2 Amplitude', fontsize=22)
+    ax4.set_xlim([time.min(), time.max()])
+    ax4.xaxis.set_tick_params(labelsize=16)
+    ax4.yaxis.set_tick_params(labelsize=16)
+
     plt.show()
 
+
+def generate_line_plot(x, y, z, ax):
+    """Return line plot with categorical coloring."""
+    points = np.array([x, y]).T.reshape(-1, 1, 2)
+    segments = np.concatenate([points[:-1], points[1:]], axis=1)
+    cmap = ListedColormap(['k', 'r', 'b', 'g'])
+    norm = BoundaryNorm([0, 1, 2, 3, 4], cmap.N)
+    lc = LineCollection(segments, cmap=cmap, norm=norm)
+    lc.set_array(z)
+    lc.set_linewidth(2)
+    ax.add_collection(lc)
+    ax.set_xlim(x.min(), x.max())
+    ax.set_ylim(y.min(), y.max())
+    custom_lines = [Line2D([0], [0], color='k', lw=4), Line2D([0], [0], color='r', lw=4),
+                    Line2D([0], [0], color='b', lw=4), Line2D([0], [0], color='g', lw=4)]
+    ax.legend(custom_lines, ['NA', 'P-Wave', 'QRS-Wave', 'T-Wave'], frameon=False,
+              fontsize=12, ncol=4, bbox_to_anchor=(1.013, 1.12))
 
 
 def plot_records():
